4c68784f676d1cabc66b6f4ea4c9bb36f933f1e2
Frankenstein <127662411+mamingladiator@users.noreply.github.com>
2023-03-13 14:44:29 +0200

    initials.py
    My first project on python.

---
+++ initials.py
@@ -0,0 +1,9 @@
+# Name: Andrew Marchenko
+# Fun fact: I like girls
+
+print ("  A    M   M")
+print (" A A   MM MM")
+print ("A   A  MM MM")
+print ("AAAAA  M M M")
+print ("A   A  M   M")
+print ("A   A  M   M")
